a2945a5ee5c36c208194ed9b0446aab8dc9a1d0f
ZockerAxel <zocker7899@gmail.com>
2024-08-12 02:59:09 +0200

    Fix local field never being assigned

--- js/viewer/Viewer.js
+++ js/viewer/Viewer.js
@@ -35,6 +35,7 @@ export default class Viewer {
         
         this.#mode = mode;
         this.#serverSelectMode = serverSelectMode;
+        this.#serverSelectModeOptions = serverSelectModeOptions;
         
         this.#registerEvents();
     }
