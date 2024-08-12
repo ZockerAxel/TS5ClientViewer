f11093af8446f118134985ae81623fa298b4c763
ZockerAxel <zocker7899@gmail.com>
2024-08-13 01:17:04 +0200

    Fix Built URL not being URI-encoded

--- js/interface/Interface.js
+++ js/interface/Interface.js
@@ -198,7 +198,7 @@ export default class Interface {
         
         url += urlComponents.join("&");
         
-        interfaceGeneratedUrl.textContent = url;
+        interfaceGeneratedUrl.textContent = encodeURI(url);
     }
     
     /**
