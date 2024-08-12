2ab9b8c8787f96c458af289fd1df9877e33afafb
ZockerAxel <zocker7899@gmail.com>
2024-08-12 20:06:56 +0200

    Move interface elements to preloaded elements

--- js/PreloadedElements.js
+++ js/PreloadedElements.js
@@ -6,6 +6,78 @@ if(!viewerDiv) throw new Error("Viewer not found in Document!");
 export const interfaceDiv = document.querySelector("#interface");
 if(!interfaceDiv) throw new Error("Interface not found in Document!");
 
-/**@type {HTMLDivElement} */
+/**@type {HTMLInputElement} */
+export const interfaceUseCustomId = document.querySelector("#use_custom_id");
+if(!interfaceUseCustomId) throw new Error("Use Custom ID not found in Document!");
+
+/**@type {HTMLInputElement} */
+export const interfaceCustomAppId = document.querySelector("#app_custom_id");
+if(!interfaceCustomAppId) throw new Error("Custom App ID not found in Document!");
+
+/**@type {HTMLInputElement} */
+export const interfaceAppPort = document.querySelector("#app_port");
+if(!interfaceAppPort) throw new Error("App Port not found in Document!");
+
+/**@type {HTMLSelectElement} */
+export const interfaceDisplayMode = document.querySelector("#display_mode");
+if(!interfaceDisplayMode) throw new Error("Display Mode not found in Document!");
+
+/**@type {HTMLSelectElement} */
+export const interfaceViewerMode = document.querySelector("#mode");
+if(!interfaceViewerMode) throw new Error("Viwer Mode not found in Document!");
+
+/**@type {HTMLSelectElement} */
+export const interfaceServer = document.querySelector("#server");
+if(!interfaceServer) throw new Error("Server not found in Document!");
+
+/**@type {HTMLInputElement} */
+export const interfaceServerName = document.querySelector("#server_name");
+if(!interfaceServerName) throw new Error("Server Name not found in Document!");
+
+/**@type {HTMLSelectElement} */
 export const interfaceServerList = document.querySelector("#server_list");
 if(!interfaceServerList) throw new Error("Server List not found in Document!");
+
+/**@type {HTMLInputElement} */
+export const interfaceAlignment = document.querySelector("#alignment");
+if(!interfaceAlignment) throw new Error("Alignment not found in Document!");
+
+/**@type {HTMLInputElement} */
+export const interfaceHideChannel = document.querySelector("#hide_channel");
+if(!interfaceHideChannel) throw new Error("Hide Channel not found in Document!");
+
+/**@type {HTMLInputElement} */
+export const interfaceHideStatus = document.querySelector("#hide_status");
+if(!interfaceHideStatus) throw new Error("Hide Status not found in Document!");
+
+/**@type {HTMLInputElement} */
+export const interfaceOnlyTalking = document.querySelector("#only_talking");
+if(!interfaceOnlyTalking) throw new Error("Only Talking not found in Document!");
+
+/**@type {HTMLInputElement} */
+export const interfaceShowAvatars = document.querySelector("#show_avatars");
+if(!interfaceShowAvatars) throw new Error("Show Avatars not found in Document!");
+
+/**@type {HTMLInputElement} */
+export const interfaceHideEmpty = document.querySelector("#hide_empty");
+if(!interfaceHideEmpty) throw new Error("Hide Empty not found in Document!");
+
+/**@type {HTMLInputElement} */
+export const interfaceShowSpacers = document.querySelector("#show_spacers");
+if(!interfaceShowSpacers) throw new Error("Show Spacers not found in Document!");
+
+/**@type {HTMLInputElement} */
+export const interfaceDisableLocalClientColor = document.querySelector("#disable_local_client_color");
+if(!interfaceDisableLocalClientColor) throw new Error("Disable Local Client Color not found in Document!");
+
+/**@type {HTMLInputElement} */
+export const interfaceShowQueryClients = document.querySelector("#show_query_clients");
+if(!interfaceShowQueryClients) throw new Error("Show Query Clients not found in Document!");
+
+/**@type {HTMLInputElement} */
+export const interfaceScaleSlider = document.querySelector("#scale_slider");
+if(!interfaceScaleSlider) throw new Error("Scale Slider not found in Document!");
+
+/**@type {HTMLInputElement} */
+export const interfaceScale = document.querySelector("#scale");
+if(!interfaceScale) throw new Error("Scale not found in Document!");
--- js/interface/Interface.js
+++ js/interface/Interface.js
@@ -1,5 +1,5 @@
 //@ts-check
-import { interfaceDiv, interfaceServerList } from "../PreloadedElements.js";
+import { interfaceCustomAppId, interfaceDiv, interfaceHideChannel, interfaceScale, interfaceScaleSlider, interfaceServer, interfaceServerList, interfaceServerName, interfaceUseCustomId, interfaceViewerMode } from "../PreloadedElements.js";
 import Handler from "../ts/Handler.js";
 import Server from "../ts/Server.js";
 import Viewer from "../viewer/Viewer.js";
@@ -29,6 +29,39 @@ export default class Interface {
         
         this.#handler.onNewServer(function(server) {
             self.#registerServer(server);
+        });
+        
+        this.#initDynamicInterface();
+    }
+    
+    #initDynamicInterface() {
+        interfaceUseCustomId.addEventListener("change", function() {
+            interfaceCustomAppId.disabled = !interfaceUseCustomId.checked;
+        });
+        
+        interfaceServer.addEventListener("change", function() {
+            const serverNameChoosable = interfaceServer.value === "by_name";
+            
+            interfaceServerName.disabled = !serverNameChoosable;
+            interfaceServerList.disabled = !serverNameChoosable;
+        });
+        
+        interfaceServerList.addEventListener("change", function() {
+            interfaceServerName.value = interfaceServerList.value;
+        });
+        
+        interfaceViewerMode.addEventListener("change", function() {
+            const channelHideable = interfaceViewerMode.value === "channel";
+            
+            interfaceHideChannel.disabled = !channelHideable;
+        });
+        
+        interfaceScaleSlider.addEventListener("input", function() {
+            interfaceScale.value = interfaceScaleSlider.value;
+        });
+        
+        interfaceScale.addEventListener("input", function() {
+            interfaceScaleSlider.value = `${Math.max(0, Math.min(4, Number.parseFloat(interfaceScale.value)))}`;
         });
     }
     
