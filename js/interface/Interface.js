91b986d0f1be1aee24b6658b5a21caaf12eac905
ZockerAxel <zocker7899@gmail.com>
2024-08-13 23:39:49 +0200

    Add prefill of hide channel

--- js/interface/Interface.js
+++ js/interface/Interface.js
@@ -36,6 +36,9 @@ export default class Interface {
         
         interfaceAlignment.value = alignment;
         
+        interfaceHideChannel.checked = channelHidden;
+        
+        
         //Init
         this.#init();
     }
