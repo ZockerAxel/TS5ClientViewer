7afde71a4539776efaf9dcb1e406fdde3326a35f
ZockerAxel <zocker7899@gmail.com>
2024-08-13 15:49:04 +0200

    Increase Versions

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 const pageDomain = "ts5viewer.endercentral.eu";
-const cacheVersion = 1;
+const cacheVersion = 2;
 const beta = pageDomain !== location.hostname;
 const currentCacheKey = `v${cacheVersion}${beta ? "-beta" : ""}`;
 
