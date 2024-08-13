e490a85a28789651be1add8cccd8f89c06586f3e
ZockerAxel <zocker7899@gmail.com>
2024-08-14 00:50:40 +0200

    Add onViewerUpdate method to View

--- js/viewer/View.js
+++ js/viewer/View.js
@@ -3,4 +3,8 @@ export default class View {
     propagateViewerUpdate() {
         throw new Error("Not Implemented: propagateViewerUpdate()");
     }
+    
+    onViewerUpdate() {
+        //Do Nothing
+    }
 }
