222a754c93c753ce30f49ce4fa22aee1669d540d
ZockerAxel <zocker7899@gmail.com>
2024-08-16 04:34:13 +0200

    Bump App Version to 1.3.0

--- js/App.js
+++ js/App.js
@@ -3,7 +3,7 @@ import interfaceAppObject from "./data_loaders/InterfaceAppLoader.js";
 import standaloneAppObject from "./data_loaders/StandaloneAppLoader.js";
 import obsAppObject from "./data_loaders/ObsAppLoader.js";
 
-export const APP_VERSION = "1.2.0";
+export const APP_VERSION = "1.3.0";
 
 export default class App {
     #identifier;
